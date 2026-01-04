f3cc71020e165af5f51ce14efef18415c2241a6a
krishna Yadav <krishnazadav5747462@gmail.com>
2026-01-04 21:38:38 +0545

    adding more

---
+++ numpy/numpy_practises/Question_01.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+# a) 1-D array of zeros with 10 elements
+zeros = np.zeros(10)
+
+# b) 1-D array of vowels
+vowels = np.array(['a', 'e', 'i', 'o', 'u'])
+
+# c) 2-D array of ones with 2 rows and 5 columns, dtype int
+ones = np.ones((2, 5), dtype=int)
+
+# Display the arrays
+print("zeros:", zeros)
+print("vowels:", vowels)
+print("ones:\n", ones)
